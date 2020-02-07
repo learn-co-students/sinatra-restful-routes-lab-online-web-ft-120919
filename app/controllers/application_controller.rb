a90d60ce34a73cb81a1f03cc8f0022aa8200e267
ben20262 <ben20262@aim.com>
2020-02-07 10:43:32 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
@@ -5,5 +6,45 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get "/recipes/new" do
+    erb :new
+  end
+
+  post '/recipes' do
+    recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    redirect "/recipes/#{recipe.id}"
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+    recipe = Recipe.find_by_id(params[:id])
+    hash = {}
+    params.each_pair do |key, value|
+      hash[key] = value if key != :id && value != nil
+    end
+    recipe.update(hash)
+    redirect "/recipes/#{params[:id]}"
+  end
+
+  delete '/recipes/:id' do
+    recipe = Recipe.find_by_id(params[:id])
+    recipe.delete
+    redirect '/recipes'
+  end
 
 end
