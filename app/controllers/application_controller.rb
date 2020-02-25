ef6db24492acb183ad2bf5542509671f9cfeb9c3
Uchoosecode <awittyl44@gmail.com>
2020-02-24 21:04:45 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,13 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+  get '/' do
+    redirect '/recipes'
+  end
 
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index	 
+  end	
+    
 end
